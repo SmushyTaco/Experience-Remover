eade6457a19d2d80d26ddc56b5271dcd95f8a96b
SmushyTaco <smushytaco@gmail.com>
2021-05-14 21:38:22 -0700

    Dependency Updates

--- Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveEnchantmentTableFunctionality.java
+++ Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveEnchantmentTableFunctionality.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(EnchantingTableBlock.class)
-public class RemoveEnchantmentTableFunctionality {
+public abstract class RemoveEnchantmentTableFunctionality {
     @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
     private void hookOnUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         if (ExperienceRemover.INSTANCE.getConfig().getDisableMod()) return;
