d82cd1ef36b1b1f4e7f5323b2867721da57f08e4
SmushyTaco <smushytaco@gmail.com>
2022-08-14 01:54:25 -0700

    1.19.2

--- Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAlreadyRemovedLogSpam.java
+++ Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAlreadyRemovedLogSpam.java
@@ -1,5 +1,4 @@
 package com.smushytaco.experience_remover.mixins;
-
 import com.smushytaco.experience_remover.ExperienceRemover;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ExperienceOrbEntity;
@@ -8,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 @Mixin(ServerWorld.class)
 public abstract class RemoveAlreadyRemovedLogSpam {
     @Inject(method = "addEntity", at = @At("HEAD"), cancellable = true)
