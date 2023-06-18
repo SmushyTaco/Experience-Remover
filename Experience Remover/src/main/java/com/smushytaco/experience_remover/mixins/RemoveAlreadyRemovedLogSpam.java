a2ff4d5bbe8fb877f27445563cb8add9f7839a0d
Nikan Radan <github.com@alias.nikanradan.com>
2023-06-18 14:46:08 -0700

    1.20.1 Properly

--- Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAlreadyRemovedLogSpam.java
+++ Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAlreadyRemovedLogSpam.java
@@ -1,17 +1,15 @@
 package com.smushytaco.experience_remover.mixins;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import com.smushytaco.experience_remover.ExperienceRemover;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.server.world.ServerWorld;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(ServerWorld.class)
 public abstract class RemoveAlreadyRemovedLogSpam {
-    @ModifyReturnValue(method = "addEntity", at = @At("RETURN"))
+    @WrapWithCondition(method = "addEntity", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", remap = false))
     @SuppressWarnings("unused")
-    private boolean hookOnUse(boolean original, Entity entity) {
-        if (ExperienceRemover.INSTANCE.getConfig().getDisableMod() || !(entity instanceof ExperienceOrbEntity)) return original;
-        return false;
-    }
+    private boolean hookOnUse(Logger instance, String var1, Object var2, Entity entity) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || !(entity instanceof ExperienceOrbEntity); }
 }
--- Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAnvilFunctionality.java
+++ Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAnvilFunctionality.java
@@ -1,22 +1,29 @@
 package com.smushytaco.experience_remover.mixins;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import com.smushytaco.experience_remover.ExperienceRemover;
 import net.minecraft.block.AnvilBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(AnvilBlock.class)
 public abstract class RemoveAnvilFunctionality {
     @ModifyReturnValue(method = "onUse", at = @At("RETURN"))
     @SuppressWarnings("unused")
-    private ActionResult hookOnUse(ActionResult original, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (ExperienceRemover.INSTANCE.getConfig().getDisableMod()) return original;
-        return ActionResult.FAIL;
-    }
+    private ActionResult hookOnUse(ActionResult original, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowAnvilUse() || player.isCreative() ? original : ActionResult.FAIL; }
+    @WrapWithCondition(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;openHandledScreen(Lnet/minecraft/screen/NamedScreenHandlerFactory;)Ljava/util/OptionalInt;"))
+    @SuppressWarnings("unused")
+    private boolean hookOnUseOpenHandledScreen(PlayerEntity instance, @Nullable NamedScreenHandlerFactory factory) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowAnvilUse() || instance.isCreative(); }
+    @WrapWithCondition(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;incrementStat(Lnet/minecraft/util/Identifier;)V"))
+    @SuppressWarnings("unused")
+    private boolean hookOnUseIncrementStat(PlayerEntity instance, Identifier stat) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowAnvilUse() || instance.isCreative(); }
 }
