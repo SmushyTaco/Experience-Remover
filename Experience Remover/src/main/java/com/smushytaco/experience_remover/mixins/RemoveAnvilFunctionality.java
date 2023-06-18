c3f978e26837ddd202e4d261f88821f9fda03e56
Nikan Radan <github.com@alias.nikanradan.com>
2023-06-18 12:57:28 -0700

    1.20.1

--- Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAnvilFunctionality.java
+++ Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAnvilFunctionality.java
@@ -1,4 +1,5 @@
 package com.smushytaco.experience_remover.mixins;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.experience_remover.ExperienceRemover;
 import net.minecraft.block.AnvilBlock;
 import net.minecraft.block.BlockState;
@@ -10,13 +11,12 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(AnvilBlock.class)
 public abstract class RemoveAnvilFunctionality {
-    @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
-    private void hookOnUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-        if (ExperienceRemover.INSTANCE.getConfig().getDisableMod()) return;
-        cir.setReturnValue(ActionResult.FAIL);
+    @ModifyReturnValue(method = "onUse", at = @At("RETURN"))
+    @SuppressWarnings("unused")
+    private ActionResult hookOnUse(ActionResult original, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (ExperienceRemover.INSTANCE.getConfig().getDisableMod()) return original;
+        return ActionResult.FAIL;
     }
 }
