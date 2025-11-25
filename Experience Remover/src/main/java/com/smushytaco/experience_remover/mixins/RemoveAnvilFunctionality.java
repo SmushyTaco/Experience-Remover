9d1808dd5cbe84df567aefd9292a27277c09e782
Nikan Radan <github.com@alias.nikanradan.com>
2025-11-25 15:27:20 -0800

    Build Overhaul

--- Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAnvilFunctionality.java
+++ Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveAnvilFunctionality.java
@@ -1,6 +1,8 @@
 package com.smushytaco.experience_remover.mixins;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.experience_remover.ExperienceRemover;
 import net.minecraft.block.AnvilBlock;
 import net.minecraft.block.BlockState;
@@ -11,15 +13,18 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import java.util.OptionalInt;
 @Mixin(AnvilBlock.class)
 public abstract class RemoveAnvilFunctionality {
     @ModifyReturnValue(method = "onUse", at = @At("RETURN"))
     private ActionResult hookOnUse(ActionResult original, BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowAnvilUse() || player.isCreative() ? original : ActionResult.FAIL; }
-    @WrapWithCondition(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;openHandledScreen(Lnet/minecraft/screen/NamedScreenHandlerFactory;)Ljava/util/OptionalInt;"))
-    private boolean hookOnUseOpenHandledScreen(PlayerEntity instance, @Nullable NamedScreenHandlerFactory factory) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowAnvilUse() || instance.isCreative(); }
+    @WrapOperation(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;openHandledScreen(Lnet/minecraft/screen/NamedScreenHandlerFactory;)Ljava/util/OptionalInt;"))
+    private OptionalInt hookOnUseOpenHandledScreen(PlayerEntity instance, NamedScreenHandlerFactory factory, Operation<OptionalInt> original) {
+        if (ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowAnvilUse() || instance.isCreative()) return original.call(instance, factory);
+        return OptionalInt.empty();
+    }
     @WrapWithCondition(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;incrementStat(Lnet/minecraft/util/Identifier;)V"))
     private boolean hookOnUseIncrementStat(PlayerEntity instance, Identifier stat) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowAnvilUse() || instance.isCreative(); }
 }
--- Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveEnchantmentTableFunctionality.java
+++ Experience Remover/src/main/java/com/smushytaco/experience_remover/mixins/RemoveEnchantmentTableFunctionality.java
@@ -1,6 +1,7 @@
 package com.smushytaco.experience_remover.mixins;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.smushytaco.experience_remover.ExperienceRemover;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.EnchantingTableBlock;
@@ -10,13 +11,16 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import java.util.OptionalInt;
 @Mixin(EnchantingTableBlock.class)
 public abstract class RemoveEnchantmentTableFunctionality {
     @ModifyReturnValue(method = "onUse", at = @At("RETURN"))
     private ActionResult hookOnUse(ActionResult original, BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowEnchantmentTableUse() || player.isCreative() ? original : ActionResult.FAIL; }
-    @WrapWithCondition(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;openHandledScreen(Lnet/minecraft/screen/NamedScreenHandlerFactory;)Ljava/util/OptionalInt;"))
-    private boolean hookOnUseOpenHandledScreen(PlayerEntity instance, @Nullable NamedScreenHandlerFactory factory) { return ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowEnchantmentTableUse() || instance.isCreative(); }
+    @WrapOperation(method = "onUse", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;openHandledScreen(Lnet/minecraft/screen/NamedScreenHandlerFactory;)Ljava/util/OptionalInt;"))
+    private OptionalInt hookOnUseOpenHandledScreen(PlayerEntity instance, NamedScreenHandlerFactory factory, Operation<OptionalInt> original) {
+        if (ExperienceRemover.INSTANCE.getConfig().getDisableMod() || ExperienceRemover.INSTANCE.getConfig().getAllowEnchantmentTableUse() || instance.isCreative()) return original.call(instance, factory);
+        return OptionalInt.empty();
+    }
 }
